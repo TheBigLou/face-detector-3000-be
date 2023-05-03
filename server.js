c6e7b9bf47b9c26ae0386284e85d01ebe0e9cb86
Louis Hellman <louis.hellman@gmail.com>
2023-05-03 16:45:58 -0700

    fix db connectionString

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || "8080";
 const db = require('knex')({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL
-    //   port : 5432,
-    //   user : 'postgres',
-    //   password : 'VZjsNNsNCGMuLfB',
-    //   database : 'face-detector-3000-be-db'
+      connectionString : process.env.DATABASE_URL,
+      port : 5432,
+      user : 'postgres',
+      password : 'VZjsNNsNCGMuLfB',
+      database : 'face-detector-3000-be-db'
     }
 });
 

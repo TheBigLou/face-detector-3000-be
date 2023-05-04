5e85cfce3566b7d1744fb8a3259d10db96fc07ff
Louis Hellman <louis.hellman@gmail.com>
2023-05-03 21:02:12 -0700

    updated db

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ const port = process.env.PORT || "8080";
 
 const db = require('knex')({
     client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL || 'postgres://postgres:VZjsNNsNCGMuLfB@face-detector-3000-be-db.flycast:5432',
-      database: 'face-detector-3000-be-db'
-    }
+    connection: process.env.DATABASE_URL
 });
 
 // controllers //
